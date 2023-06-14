5d3a41088479c592d323922d1d6c881f6731cb51
Ivan Mijailovic <ivan.mijailovic994@gmail.com>
2023-06-14 12:28:22 +0200

    Create nav bar and account view section with table

---
+++ app.js
@@ -0,0 +1,1 @@
+let accountsView = document.querySelector(".accountView")
---
+++ db.js
@@ -0,0 +1,26 @@
+let db = [
+    {
+        id : "1",
+        fullName : "Ivan",
+        deposit : 15000, 
+        card : "Master"
+    },
+    {
+        id : "2",
+        fullName : "Katarina",
+        deposit : 50000, 
+        card : "Master"
+    },
+    {
+        id : "3",
+        fullName : "Stefan",
+        deposit : 40000, 
+        card : "Visa"
+    },
+    {
+        id : "4",
+        fullName : "Tijana",
+        deposit : 20000, 
+        card : "Dina"
+    }
+]
