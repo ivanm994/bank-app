88bac99f963bcc7b9fc5d16dcd462d603ef61ae2
Ivan Mijailovic <ivan.mijailovic994@gmail.com>
2023-06-14 12:52:17 +0200

    Create accounts table and fill it using crateAccountTAble function

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-let accountsView = document.querySelector(".accountView")
+let accountsView = document.querySelector(".accountView");
+let accountsTable = accountsView.querySelector("tbody");
+
+createAccountTable();
+
+function createAccountTable(){
+    let html = ""
+    db.forEach((account) => {
+        html += `
+            <tr>
+                <td>${account.id}</td>
+                <td>${account.fullName}</td>
+                <td>${account.deposit}</td>
+                <td>${account.card}</td>
+            </tr>
+        `.trim()
+    });
+accountsTable.innerHTML = html;
+
+}
